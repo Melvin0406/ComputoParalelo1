7bbf9700287c2a14c08081e937c35e9fc590fef6
Melvin0406 <kevinb062004@gmail.com>
2025-08-14 12:31:53 -0700

    Initial commit

---
+++ Containerfile
@@ -0,0 +1,32 @@
+FROM fedora:42
+
+RUN dnf update -y
+RUN dnf install -y gcc vim gcc gcc-c++ clang openssh-server lldb clangd libicu python3 pip cmake perl gdb awk npm git
+RUN pip install conan 
+RUN pip install ninja
+RUN pip install meson
+
+RUN conan profile detect
+RUN conan profile detect --name clang
+RUN sed -i 's/compiler=gcc/compiler=clang/' root/.conan2/profiles/clang
+RUN sed -i 's/compiler.version=.*/compiler.version=20/' root/.conan2/profiles/clang
+RUN echo [buildenv] >> root/.conan2/profiles/clang
+RUN echo CC=clang >> root/.conan2/profiles/clang
+RUN echo CXX=clang++ >> root/.conan2/profiles/clang
+
+RUN sed -i 's#Port 22#Port 22#g' /etc/ssh/sshd_config
+RUN sed -i 's#PermitRootLogin.*#PermitRootLogin yes#g' /etc/ssh/sshd_config
+
+RUN ssh-keygen -A
+RUN touch ~/.ssh/authorized_keys && chmod 600 ~/.ssh/authorized_keys
+
+RUN mkdir /devenv && mkdir /devenv/workspace && mkdir /devenv/workspace/service
+
+#copy public key into container for autmatic login
+COPY id_ed25519.pub /devenv
+
+RUN cat devenv/id_ed25519.pub >> ~/.ssh/authorized_keys
+
+EXPOSE 22 8080
+
+CMD ["/usr/sbin/sshd", "-D"]
---
+++ Philosophers/main.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+#include <memory>
+#include <string>
+#include <thread>
+#include <mutex>
+#include <vector>
+#include <chrono>
+#include <print>
+
+class Fork {
+    std::mutex mtx;
+
+public:
+    void pickUp() {
+        mtx.lock();
+    }
+
+    void putDown() {
+        mtx.unlock();
+    }
+};
+
+class Philosopher {
+    std::shared_ptr<Fork> leftFork;
+    std::shared_ptr<Fork> rightFork;
+    std::string name;
+
+public:
+    explicit Philosopher(std::string name) : name(std::move(name)) {}
+
+    void eat() {
+        while (true) {
+            leftFork->pickUp();
+            // std::println("{} picked up left fork", name);
+            // std::this_thread::sleep_for(std::chrono::seconds(1));
+            rightFork->pickUp();
+            // std::println("{} picked up right fork", name);
+
+            std::println("{} started eating", name);
+            std::this_thread::sleep_for(std::chrono::seconds(1));
+            std::println("{} finished eating", name);
+
+            leftFork->putDown();
+            rightFork->putDown();
+
+            std::this_thread::sleep_for(std::chrono::seconds(1));
+        }
+    }
+
+    std::shared_ptr<Fork> &LeftFork() { return leftFork; }
+    std::shared_ptr<Fork> &RightFork() { return rightFork; }
+};
+
+int main() {
+    const int numPhilosophers = 5;
+    std::vector<std::shared_ptr<Fork>> forks(numPhilosophers);
+    std::vector<Philosopher> philosophers;
+
+    for (int i = 0; i < numPhilosophers; i++) {
+        forks[i] = std::make_shared<Fork>();
+    }
+
+    for (int i = 0; i < numPhilosophers; i++) {
+        philosophers.emplace_back("Shrek " + std::to_string(i + 1));
+        philosophers[i].LeftFork() = forks[i];
+        philosophers[i].RightFork() = forks[(i + 1) % numPhilosophers];
+    }
+
+    std::vector<std::thread> threads;
+    for (auto &p : philosophers) {
+        threads.emplace_back(&Philosopher::eat, &p);
+    }
+
+    for (auto &t : threads) {
+        t.join();
+    }
+
+    return 0;
+}
